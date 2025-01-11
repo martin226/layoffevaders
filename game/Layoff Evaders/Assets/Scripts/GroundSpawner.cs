75167fee17686a14d018f63a4d757edc3ba3dc22
Martin Sit <46298951+martin226@users.noreply.github.com>
2025-01-11 14:04:24 -0500

    feat: trying to improve appearance

--- game/Layoff Evaders/Assets/Scripts/GroundSpawner.cs
+++ game/Layoff Evaders/Assets/Scripts/GroundSpawner.cs
@@ -12,7 +12,10 @@ public class GroundSpawner : MonoBehaviour
         Vector3 spawnLeft = new Vector3(nextSpawnPoint.x - 20,nextSpawnPoint.y,nextSpawnPoint.z);
         Vector3 spawnRight = new Vector3(nextSpawnPoint.x + 20,nextSpawnPoint.y,nextSpawnPoint.z);
         GameObject temp2 = Instantiate(environmentTile[0],spawnLeft,Quaternion.identity);
+        // GameObject temp3 = Instantiate(environmentTile[0],spawnRight,Quaternion.identity);
+        // instantiate temp3 but flip it horizontally
         GameObject temp3 = Instantiate(environmentTile[0],spawnRight,Quaternion.identity);
+        temp3.transform.localScale = new Vector3(-1,1,1);
         temp2.transform.SetParent(temp.transform);
         temp3.transform.SetParent(temp.transform);
         nextSpawnPoint = temp.transform.GetChild(1).transform.position;
