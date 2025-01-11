65a4b3178bdeabc2e06f28e7d358eb2ad3776ac3
Andrew Chu <67875282+andrewchu16@users.noreply.github.com>
2025-01-11 18:51:56 -0500

    Add firebase manager

---
+++ web/firebase_manager.py
@@ -0,0 +1,44 @@
+from collections import defaultdict
+from collections.abc import Callable
+import json
+import threading
+import firebase_admin
+from firebase_admin import credentials, db
+
+
+# Initialize Firebase
+cred = credentials.Certificate("service-account.json")
+firebase_admin.initialize_app(cred, options={
+    'databaseURL': "https://layoff-evaders-default-rtdb.firebaseio.com/",
+})
+
+class FirebaseManager:
+    def __init__(self, user="user1"):
+        self.user = user
+        self.threads = []
+        self.stop_event = threading.Event()
+                
+    def get_user_data(self):
+        """Get a snapshot of the entire user's data from Firebase."""
+        ref = db.reference(f"/users/{self.user}")
+        return ref.get()
+        
+    def close(self):
+        """Close the Firebase manager."""
+        if self.stop_event and self.threads:
+            self.stop_event.set()
+            for thread in self.threads:
+                thread.join()
+    
+    def listen(self, path: str, callback: Callable):
+        """Listen for changes in the user's data. path should NOT start with a slash. Will give you the value of the data at the path everytime it updates, in the correct datatype."""
+        ref = db.reference(f"/users/{self.user}/{path}")
+        
+        def run():
+            self.stop_event.clear()
+            ref.listen(lambda event: callback(event.data))
+        
+        self.threads.append(threading.Thread(target=run, daemon=True))
+        self.threads[-1].start()
+        
+
